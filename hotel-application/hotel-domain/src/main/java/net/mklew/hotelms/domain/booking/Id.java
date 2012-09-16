e7509af6302526843e7c174e4959cb28152ad6a1
Marek Lewandowski <mklew@github>
2012-09-16 16:26:16 +0200

    Add Id class.
    Immutable, encapsulates String used as identificatin. Gives special meaning to that String.

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/Id.java
@@ -0,0 +1,22 @@
+package net.mklew.hotelms.domain.booking;
+
+/**
+ * Id class gives special meaning. Better than using String
+ * @author: Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since: 9/9/12
+ * time 8:32 PM
+ */
+public final class Id
+{
+    private final String id;
+
+    public Id(String id)
+    {
+        this.id = id;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+}
