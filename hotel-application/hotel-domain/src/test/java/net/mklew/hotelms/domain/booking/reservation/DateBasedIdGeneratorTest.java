c4269d0b7a48f7880a91923e01e81dc31139aac7
Marek Lewandowski <mklew@github>
2012-09-20 21:20:28 +0200

    Introduced interface for IdGenerator

--- hotel-application/hotel-domain/src/test/java/net/mklew/hotelms/domain/booking/reservation/DateBasedIdGeneratorTest.java
+++ hotel-application/hotel-domain/src/test/java/net/mklew/hotelms/domain/booking/reservation/DateBasedIdGeneratorTest.java
@@ -1,8 +1,5 @@
 package net.mklew.hotelms.domain.booking.reservation;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 import org.joda.time.DateTime;
 import org.testng.annotations.Test;
 import static org.fest.assertions.Assertions.assertThat;
@@ -12,17 +9,17 @@ import static org.fest.assertions.Assertions.assertThat;
  * @since: 9/16/12
  * time 7:00 PM
  */
-public class IdGeneratorTest
+public class DateBasedIdGeneratorTest
 {
     @Test
     public void should_generate_at_least_8_digit_ids()
     {
-        DateTime dateTime = new DateTime(2012, 03, 20, 15, 55);
-        IdGenerator idGenerator = new IdGenerator(dateTime);
+        DateTime dateTime = new DateTime(2012, 3, 20, 15, 55);
+        IdGenerator dateBasedIdGenerator = new DateBasedIdGenerator(dateTime);
 
         for(int i = 0 ; i < 99_000_000 ; i++)
         {
-            Long actual = idGenerator.generateId(i);
+            Long actual = dateBasedIdGenerator.generateId(i);
             assertThat(actual).isGreaterThanOrEqualTo(10_000_000);
         }
     }
