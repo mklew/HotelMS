9810b972b73b1581470d7776ccf23af818f006f1
Marek Lewandowski <mklew@github>
2012-09-27 12:57:01 -0700

    Add room name class description

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/room/RoomName.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/room/RoomName.java
@@ -4,6 +4,9 @@ package net.mklew.hotelms.domain.room;
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 9/27/12
  *        Time: 11:39 AM
+ *
+ *  Room name consists of some kind of prefix, delimeter (or not - can be empty ) and actual name where
+ *  name can be room number or some other sort of business identification
  */
 public class RoomName
 {
