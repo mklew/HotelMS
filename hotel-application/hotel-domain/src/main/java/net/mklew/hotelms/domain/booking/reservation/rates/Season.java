6776059cd2fe621f41329367539222a992109506
Marek Lewandowski <mklew@github>
2012-09-27 12:00:03 -0700

    Add Season

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Season.java
@@ -0,0 +1,10 @@
+package net.mklew.hotelms.domain.booking.reservation.rates;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 11:59 AM
+ */
+public class Season
+{
+}
