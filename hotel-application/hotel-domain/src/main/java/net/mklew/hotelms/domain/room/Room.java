52337c59f01e4d0301c786fda074d3a873122320
Marek Lewandowski <mklew@github>
2012-09-27 11:29:12 -0700

    Add room class

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/room/Room.java
@@ -0,0 +1,16 @@
+package net.mklew.hotelms.domain.room;
+
+import net.mklew.hotelms.domain.booking.reservation.rates.RackRate;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 11:27 AM
+ */
+public class Room
+{
+    private RackRate rackRate;
+
+
+
+}
