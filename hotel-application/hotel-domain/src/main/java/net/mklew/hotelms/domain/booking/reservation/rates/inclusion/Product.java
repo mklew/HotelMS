acaff5e5c19aee757bac303012db80ee62421da2
Marek Lewandowski <mklew@github>
2012-09-27 12:44:19 -0700

    Add Product

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/inclusion/Product.java
@@ -0,0 +1,18 @@
+package net.mklew.hotelms.domain.booking.reservation.rates.inclusion;
+
+import net.mklew.hotelms.domain.money.Money;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 12:43 PM
+ *
+ *        Product is what you this it is. Products needs to be stored to be used in inclusions.
+ */
+public class Product
+{
+    private String name;
+    private String description;
+    private Money referencePrice;
+
+}
