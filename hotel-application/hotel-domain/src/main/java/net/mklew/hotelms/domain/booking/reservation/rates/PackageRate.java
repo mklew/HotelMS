fb76e489bd81a75425c3f93be8ed403aa3158a6f
Marek Lewandowski <mklew@github>
2012-09-27 12:02:53 -0700

    Implement Rate

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/PackageRate.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/PackageRate.java
@@ -1,10 +1,29 @@
 package net.mklew.hotelms.domain.booking.reservation.rates;
 
+import net.mklew.hotelms.domain.money.Money;
+
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 9/27/12
  *        Time: 12:00 PM
  */
-public class PackageRate
+public class PackageRate implements Rate
 {
+    @Override
+    public Money standardPrice()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraPerson()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraBed()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/SeasonRate.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/SeasonRate.java
@@ -1,11 +1,30 @@
 package net.mklew.hotelms.domain.booking.reservation.rates;
 
+import net.mklew.hotelms.domain.money.Money;
+
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 9/27/12
  *        Time: 11:59 AM
  */
-public class SeasonRate
+public class SeasonRate implements Rate
 {
 
+    @Override
+    public Money standardPrice()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraPerson()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraBed()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
