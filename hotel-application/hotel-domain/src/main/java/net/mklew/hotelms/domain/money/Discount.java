5b68f66f0ea29e4c96421c836ff9d876c9876b57
Marek Lewandowski <mklew@github>
2012-09-27 12:38:52 -0700

    Add Discount class

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/money/Discount.java
@@ -0,0 +1,11 @@
+package net.mklew.hotelms.domain.money;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 12:37 PM
+ */
+public class Discount
+{
+
+}
