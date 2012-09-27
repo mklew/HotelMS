a23df0b537584903a1d92669ad89ceeabe853e2c
Marek Lewandowski <mklew@github>
2012-09-27 12:21:00 -0700

    Add AvailabilityPeriod

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/AvailabilityPeriod.java
@@ -0,0 +1,14 @@
+package net.mklew.hotelms.domain.booking.reservation.rates;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 12:20 PM
+ */
+public interface AvailabilityPeriod
+{
+    boolean isAvailableIn(DateTime time);
+    boolean isAvailableNow();
+}
