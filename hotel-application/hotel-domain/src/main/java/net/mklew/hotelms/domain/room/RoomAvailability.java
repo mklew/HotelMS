f85dac63f90d5071d367c873ac5a805ebda527af
Marek Lewandowski <mklew@github>
2012-09-27 11:35:53 -0700

    Add room availability

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/room/RoomAvailability.java
@@ -0,0 +1,12 @@
+package net.mklew.hotelms.domain.room;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 11:34 AM
+ */
+public enum RoomAvailability
+{
+    AVAILABLE, OCCUPIED
+}
+
