3d2b459aec276e96f1a924db8754f8aa02753afa
Marek Lewandowski <mklew@github>
2012-09-27 11:28:59 -0700

    RackRate implements Rate methods

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/RackRate.java
@@ -0,0 +1,30 @@
+package net.mklew.hotelms.domain.booking.reservation.rates;
+
+import net.mklew.hotelms.domain.money.Money;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 11:24 AM
+ */
+public class RackRate implements Rate
+{
+
+    @Override
+    public Money standardPrice()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraPerson()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Money upchargeExtraBed()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
