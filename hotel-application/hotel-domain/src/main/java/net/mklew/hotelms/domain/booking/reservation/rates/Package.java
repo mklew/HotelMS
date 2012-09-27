6045cd7466dad3556fff669b508afe25b071c0f6
Marek Lewandowski <mklew@github>
2012-09-27 12:01:49 -0700

    Add Package class

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Package.java
@@ -0,0 +1,10 @@
+package net.mklew.hotelms.domain.booking.reservation.rates;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 12:01 PM
+ */
+public class Package
+{
+}
