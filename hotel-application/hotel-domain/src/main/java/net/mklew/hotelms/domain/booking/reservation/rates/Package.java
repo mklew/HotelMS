b48f1d0ef58ba17b5fbfbff85694ec2e12f85fc2
Marek Lewandowski <mklew@github>
2012-09-27 12:50:14 -0700

    Add Fields

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Package.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Package.java
@@ -1,5 +1,9 @@
 package net.mklew.hotelms.domain.booking.reservation.rates;
 
+import net.mklew.hotelms.domain.booking.reservation.Night;
+import net.mklew.hotelms.domain.booking.reservation.rates.inclusion.Inclusion;
+import net.mklew.hotelms.domain.policy.Policy;
+
 import java.util.List;
 
 /**
@@ -10,6 +14,14 @@ import java.util.List;
 public class Package
 {
     private List<AvailabilityPeriod> timePeriods;
+    private List<Night> nights;
+    private List<Inclusion> inclusions;
+    private List<Policy> policies;
+    private List<PackageRate> rates;
 
+    public int lengthOfStay()
+    {
+        return nights.size();
+    }
 
 }
