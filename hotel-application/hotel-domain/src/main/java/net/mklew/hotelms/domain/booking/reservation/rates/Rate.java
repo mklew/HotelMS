072a96822dc49755048ef8fd82dd632c17f6921d
Marek Lewandowski <mklew@github>
2012-09-27 11:23:49 -0700

    Add Rate, Money

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Rate.java
@@ -0,0 +1,13 @@
+package net.mklew.hotelms.domain.booking.reservation.rates;
+
+import net.mklew.hotelms.domain.money.Money;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 11:22 AM
+ */
+public interface Rate
+{
+    Money price();
+}
--- hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/AppTest.java
+++ hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/AppTest.java
@@ -1,38 +1,14 @@
 package net.mklew.hotelms.persistance;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import static org.fest.assertions.Fail.fail;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    @Test
+    public void testMe()
     {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
