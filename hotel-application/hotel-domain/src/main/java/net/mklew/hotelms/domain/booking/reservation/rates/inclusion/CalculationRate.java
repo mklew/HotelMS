13ac99ac607eb640ea778d1c0d2cdb050e569d20
Marek Lewandowski <mklew@github>
2012-09-27 12:40:33 -0700

    Add CalculationRate enum

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/inclusion/CalculationRate.java
@@ -0,0 +1,11 @@
+package net.mklew.hotelms.domain.booking.reservation.rates.inclusion;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/27/12
+ *        Time: 12:39 PM
+ */
+enum CalculationRate
+{
+    PER_PERSON, PER_ADULT, PER_CHILD, PER_BABY, PER_ROOM;
+}
