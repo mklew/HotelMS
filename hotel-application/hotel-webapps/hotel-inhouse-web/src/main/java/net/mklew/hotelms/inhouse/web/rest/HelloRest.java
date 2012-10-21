f369c35fe80a499c2204b90e80c86db68a6b8d09
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-10-21 13:27:55 -0700

    Add HelloRest service taken from ObjectLedge-HelloWorld examples

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/HelloRest.java
@@ -0,0 +1,38 @@
+package net.mklew.hotelms.inhouse.web.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 10/21/12
+ *        Time: 1:27 PM
+ */
+@Path("/hello")
+public class HelloRest
+{
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    }
+
+}
