5525e8df5f69017761eeb356fd56e7fa31298392
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-10-27 12:40:25 +0200

    Add ReservationTable view with java controller/builder and separate javascript file which is executed after fetching view.
    Probably better way of doing this would be to change that view into a dojo widget. Need to analyse pros and cons

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/modules/views/top/ReservationTable.java
@@ -0,0 +1,34 @@
+package net.mklew.hotelms.inhouse.web.modules.views.top;
+
+import org.objectledge.context.Context;
+import org.objectledge.i18n.I18nContext;
+import org.objectledge.modules.views.BasicLedgeTopView;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.HttpContext;
+import org.objectledge.web.mvc.MVCContext;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 10/25/12
+ *        Time: 8:30 PM
+ */
+public class ReservationTable extends BasicLedgeTopView
+{
+    /**
+     * Constructs a builder instance.
+     *
+     * @param context application context for use by this builder.
+     */
+    public ReservationTable(Context context)
+    {
+        super(context);
+    }
+
+    @Override
+    public void process(Parameters parameters, TemplatingContext templatingContext, MVCContext mvcContext, HttpContext httpContext, I18nContext i18nContext) throws ProcessingException
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
