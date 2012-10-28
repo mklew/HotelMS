e0943073886bab72a38919598125011c1ccc289a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-10-28 20:31:38 +0100

    Add cometD boilerplate and sample service

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/cometd/BayeuxInitializer.java
@@ -0,0 +1,29 @@
+package net.mklew.hotelms.inhouse.web.cometd;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.cometd.bayeux.server.BayeuxServer;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 10/28/12
+ *        Time: 5:45 PM
+ */
+public class BayeuxInitializer extends GenericServlet
+{
+    public void init() throws ServletException
+    {
+        BayeuxServer bayeux = (BayeuxServer) getServletContext().getAttribute(BayeuxServer.ATTRIBUTE);
+        new HelloService(bayeux);
+    }
+
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException
+    {
+        throw new ServletException();
+    }
+}
---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/cometd/HelloService.java
@@ -0,0 +1,34 @@
+package net.mklew.hotelms.inhouse.web.cometd;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.cometd.bayeux.Message;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ServerSession;
+import org.cometd.server.AbstractService;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 10/28/12
+ *        Time: 5:50 PM
+ */
+public class HelloService extends AbstractService
+{
+    public HelloService(BayeuxServer bayeux)
+    {
+        super(bayeux, "hello");
+        addService("/service/hello", "processHello");
+    }
+
+    public void processHello(ServerSession remote, Message message)
+    {
+        Map<String, Object> input = message.getDataAsMap();
+        String name = (String) input.get("name");
+
+        Map<String, Object> output = new HashMap<String, Object>();
+        output.put("greeting", "Hello, " + name);
+        remote.deliver(getServerSession(), "/hello", output, null);
+    }
+}
+
