1129f0a79fd841d4873d323d005442b96c4319c6
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-04 13:41:46 +0100

    Add statuses

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/ReservationStatus.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/ReservationStatus.java
@@ -1,11 +1,47 @@
 package net.mklew.hotelms.domain.booking;
 
 /**
- * @author: Marek Lewandowski <marek.m.lewandowski@gmail.com>
- * @since: 9/9/12
+ * Status of reservation
+ *
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/9/12
  * time 9:04 PM
  */
 public enum ReservationStatus
 {
-    // todo implement reservation status
+    // todo revisit history status, maybe rename it to something more appropriate
+    /**
+     * Checkin status means that today is the day of check-in
+     */
+    CHECKIN,
+    /**
+     * Checkout status means that today is the day of check-out
+     */
+    CHECKOUT,
+    /**
+     * In house reservation is checked-in reservation and during stay of guest/guests.
+     */
+    INHOUSE,
+    /**
+     * History reservation is the ending status of reservation usual lifecycle
+     */
+    HISTORY,
+    /**
+     * Cancelled reservation by guest or other authority
+     */
+    CANCELLED,
+    /**
+     * Reserved reservation is the one which is valid and waits for the day of check-in
+     */
+    RESERVED,
+    /**
+     *  No show is when guest has not checked-in in check-in day
+     */
+    NOSHOW,
+    /**
+     * Temporary reservation is the one which is valid only to some hour, for example to 4 pm day it's made
+     */
+    TEMPORARY
+
+
 }
