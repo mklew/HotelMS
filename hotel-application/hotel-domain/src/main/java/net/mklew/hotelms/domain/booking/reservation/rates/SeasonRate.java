700363947dda37baded99050c5ddc0290e1316c3
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-04 20:20:35 +0100

    Add fields and implement methods

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/SeasonRate.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/SeasonRate.java
@@ -11,24 +11,42 @@ import net.mklew.hotelms.domain.room.Room;
 public class SeasonRate implements Rate
 {
 
+    private Money standardPrice;
+    private Money upchargeExtraPerson;
+    private Money upchargeExtraBed;
     private Season season;
     private Room room;
+
+    public SeasonRate(Money standardPrice, Money upchargeExtraPerson, Money upchargeExtraBed, Season season, Room room)
+    {
+        this.standardPrice = standardPrice;
+        this.upchargeExtraPerson = upchargeExtraPerson;
+        this.upchargeExtraBed = upchargeExtraBed;
+        this.season = season;
+        this.room = room;
+    }
 
     @Override
     public Money standardPrice()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return standardPrice;
     }
 
     @Override
     public Money upchargeExtraPerson()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return upchargeExtraPerson;
     }
 
     @Override
     public Money upchargeExtraBed()
     {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return upchargeExtraBed;
+    }
+
+    @Override
+    public String getRateName()
+    {
+       return season.getSeasonName();
     }
 }
