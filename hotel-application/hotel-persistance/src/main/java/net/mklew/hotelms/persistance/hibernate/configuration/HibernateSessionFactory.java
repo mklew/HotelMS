26ae3816c20ecf3cd1e6600f0601a42a8996f9d8
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-04 12:58:59 +0100

    Add javadoc

--- hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/hibernate/configuration/HibernateSessionFactory.java
+++ hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/hibernate/configuration/HibernateSessionFactory.java
@@ -4,6 +4,8 @@ import org.hibernate.SessionFactory;
 import org.picocontainer.Startable;
 
 /**
+ * Provides SessionFactory as soon as container's context is up.
+ *
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 11/4/12
  *        Time: 12:10 PM
