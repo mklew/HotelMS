d0c5308468d35d538c49b5875d7f801939ad2794
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-04 12:56:33 +0100

    Add interface HibernateSessionFactory which provides access to SessionFactory. SessionFactory is available as soon as container's context is up

---
+++ hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/hibernate/configuration/NativelyConfiguredHibernateSessionFactory.java
@@ -0,0 +1,48 @@
+package net.mklew.hotelms.persistance.hibernate.configuration;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.jcontainer.dna.Logger;
+
+/**
+ * Creates SessionFactory based on default Hibernate.cfg.xml configuration found in default location.
+ *
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 11/4/12
+ *        Time: 12:12 PM
+ */
+public class NativelyConfiguredHibernateSessionFactory implements HibernateSessionFactory
+{
+    private final SessionFactory sessionFactory;
+
+    public NativelyConfiguredHibernateSessionFactory(Logger logger)
+    {
+        logger.debug("Started configuring hibernate");
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
+        serviceRegistryBuilder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = serviceRegistryBuilder.buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        logger.debug("Hibernate configured successfully");
+    }
+
+    @Override
+    public SessionFactory getSessionFactory()
+    {
+        return sessionFactory;
+    }
+
+    @Override
+    public void start()
+    {
+
+    }
+
+    @Override
+    public void stop()
+    {
+
+    }
+}
