74fb8b95b32ce4595c59625abfdd80fdce614588
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-06 23:25:11 +0100

    Make Package work with Hibernate, still minimalistic mapping just to check if Package and PackageRates work together

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Package.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/rates/Package.java
@@ -5,6 +5,7 @@ import net.mklew.hotelms.domain.booking.reservation.rates.inclusion.Inclusion;
 import net.mklew.hotelms.domain.policy.Policy;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
@@ -13,15 +14,61 @@ import java.util.List;
  */
 public class Package
 {
+    private Long id;
     private List<AvailableIn> timePeriods;
     private List<Night> nights;
     private List<Inclusion> inclusions;
     private List<Policy> policies;
-    private List<PackageRate> rates;
+    private Set<PackageRate> rates;
+    private String name;
 
     public int lengthOfStay()
     {
         return nights.size();
     }
 
+    public String getPackageName()
+    {
+        return name;
+    }
+
+    public Set<PackageRate> getRates()
+    {
+        return rates;
+    }
+
+    // hibernate
+    private Long getId()
+    {
+        return id;
+    }
+
+    // hibernate
+    private void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    // hibernate
+    private String getName()
+    {
+        return name;
+    }
+
+    // hibernate
+    private void setName(String name)
+    {
+        this.name = name;
+    }
+
+    // hibernate
+    private void setRates(Set<PackageRate> rates)
+    {
+        this.rates = rates;
+    }
+
+    // hibernate
+    Package()
+    {
+    }
 }
