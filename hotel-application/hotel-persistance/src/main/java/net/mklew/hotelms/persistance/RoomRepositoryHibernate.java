741555d204c204fea0fafdeb940938b8a55fc77a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-11 20:05:59 +0100

    Satisfy RoomRepository contract.

--- hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/RoomRepositoryHibernate.java
+++ hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/RoomRepositoryHibernate.java
@@ -3,8 +3,11 @@ package net.mklew.hotelms.persistance;
 import net.mklew.hotelms.domain.room.Room;
 import net.mklew.hotelms.domain.room.RoomName;
 import net.mklew.hotelms.domain.room.RoomRepository;
+import net.mklew.hotelms.domain.room.RoomType;
 import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
 import org.hibernate.Session;
+
+import java.util.Collection;
 
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
@@ -26,5 +29,13 @@ public class RoomRepositoryHibernate extends HibernateRepository implements Room
         Room retrievedRoom = (Room)session.byId(Room.class).load(name);
         // TODO if retrievedRoom is null then throw some meaningful exception?
         return retrievedRoom;
+    }
+
+    @Override
+    public Collection<RoomType> getAllRoomTypes()
+    {
+        Session session = getCurrentSession();
+        Collection<RoomType> roomTypes = session.createQuery("from RoomType").list();
+        return roomTypes;
     }
 }
