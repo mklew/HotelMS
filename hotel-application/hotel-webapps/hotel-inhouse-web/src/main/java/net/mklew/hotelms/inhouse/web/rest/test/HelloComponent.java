26d67d65825ea13348c14dfecab343ea8cae8708
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2012-11-24 13:29:13 +0100

    Apply to new jersey rest valve

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/test/HelloComponent.java
@@ -0,0 +1,16 @@
+package net.mklew.hotelms.inhouse.web.rest.test;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 11/18/12
+ *        Time: 8:56 PM
+ */
+public class HelloComponent
+{
+    private final String hello = "hello from HelloComponent";
+
+    public String saySomething()
+    {
+        return hello;
+    }
+}
