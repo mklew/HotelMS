02210f834c87564f4e9f0b7c039b16a541060795
Marek Lewandowski <mklew@github>
2012-11-25 15:58:31 +0100

    Add Rates resource accessible via /rest/rates/{roomNumber} which returns rates for given room.

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/Rates.java
@@ -0,0 +1,61 @@
+package net.mklew.hotelms.inhouse.web.rest;
+
+import com.sun.jersey.spi.resource.Singleton;
+import net.mklew.hotelms.domain.booking.reservation.rates.Rate;
+import net.mklew.hotelms.domain.booking.reservation.rates.RateRepository;
+import net.mklew.hotelms.domain.room.Room;
+import net.mklew.hotelms.domain.room.RoomName;
+import net.mklew.hotelms.domain.room.RoomRepository;
+import net.mklew.hotelms.inhouse.web.dto.RateDto;
+import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
+import org.hibernate.Session;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 11/24/12
+ * time 9:02 PM
+ */
+@Singleton
+@Path("rates/{roomName}")
+public class Rates
+{
+    private final RateRepository rateRepository;
+    private final RoomRepository roomRepository;
+    private final HibernateSessionFactory hibernateSessionFactory;
+
+    public Rates(RateRepository rateRepository, RoomRepository roomRepository, HibernateSessionFactory hibernateSessionFactory)
+    {
+        this.rateRepository = rateRepository;
+        this.roomRepository = roomRepository;
+        this.hibernateSessionFactory = hibernateSessionFactory;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<RateDto> getAllRates(@PathParam("roomName") String roomName)
+    {
+        Session session = hibernateSessionFactory.getCurrentSession();
+        session.beginTransaction();
+        RoomName name = new RoomName(roomName);
+        Room room = roomRepository.getRoomByName(name);
+        Collection<Rate> rates = rateRepository.getAllRatesForRoom(room);
+        ArrayList<RateDto> rateDtos = new ArrayList<>(rates.size());
+        for(Rate rate : rates)
+        {
+            rateDtos.add(RateDto.createFromRate(rate));
+        }
+        session.getTransaction().commit();
+        return rateDtos;
+    }
+
+
+}
