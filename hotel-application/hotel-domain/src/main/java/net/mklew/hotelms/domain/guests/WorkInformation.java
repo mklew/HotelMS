07abf434574ad977b25b655d6fa392a498d1f20d
Marek Lewandowski <mklew@github>
2012-12-02 17:55:24 +0100

    Add delegates. WorkDetails is now hidden.

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/WorkInformation.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/WorkInformation.java
@@ -33,4 +33,114 @@ public final class WorkInformation
     {
         return address;
     }
+
+    public String getOrganizationName()
+    {
+        return workDetails.getOrganizationName();
+    }
+
+    public String getSecondaryPhoneNumber()
+    {
+        return workDetails.getSecondaryPhoneNumber();
+    }
+
+    public void setPrimaryPhoneNumber(String primaryPhoneNumber)
+    {
+        workDetails.setPrimaryPhoneNumber(primaryPhoneNumber);
+    }
+
+    public void setDesignation(String designation)
+    {
+        workDetails.setDesignation(designation);
+    }
+
+    public String getDesignation()
+    {
+        return workDetails.getDesignation();
+    }
+
+    public void setOrganizationName(String organizationName)
+    {
+        workDetails.setOrganizationName(organizationName);
+    }
+
+    public String getPrimaryPhoneNumber()
+    {
+        return workDetails.getPrimaryPhoneNumber();
+    }
+
+    public void setSecondaryPhoneNumber(String secondaryPhoneNumber)
+    {
+        workDetails.setSecondaryPhoneNumber(secondaryPhoneNumber);
+    }
+
+    public String getRegionName()
+    {
+        return address.getRegionName();
+    }
+
+    public void setRegionName(String regionName)
+    {
+        address.setRegionName(regionName);
+    }
+
+    public String getAreaCode()
+    {
+        return address.getAreaCode();
+    }
+
+    public void setAreaCode(String areaCode)
+    {
+        address.setAreaCode(areaCode);
+    }
+
+    public String getCity()
+    {
+        return address.getCity();
+    }
+
+    public void setCity(String city)
+    {
+        address.setCity(city);
+    }
+
+    public String getStreet()
+    {
+        return address.getStreet();
+    }
+
+    public void setStreet(String street)
+    {
+        address.setStreet(street);
+    }
+
+    public String getStreetNumber()
+    {
+        return address.getStreetNumber();
+    }
+
+    public void setStreetNumber(String streetNumber)
+    {
+        address.setStreetNumber(streetNumber);
+    }
+
+    public String getApartmentNumber()
+    {
+        return address.getApartmentNumber();
+    }
+
+    public void setApartmentNumber(String apartmentNumber)
+    {
+        address.setApartmentNumber(apartmentNumber);
+    }
+
+    public AddressType getAddressType()
+    {
+        return address.getAddressType();
+    }
+
+    public void setAddressType(AddressType addressType)
+    {
+        address.setAddressType(addressType);
+    }
 }
