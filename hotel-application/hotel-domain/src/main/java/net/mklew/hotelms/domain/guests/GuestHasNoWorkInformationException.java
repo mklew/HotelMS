1012d62c035be9fa3ac4656738dce4671e47e80d
Marek Lewandowski <mklew@github>
2012-12-02 17:52:26 +0100

    Add GuestHasNoWorkInformationException

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/GuestHasNoWorkInformationException.java
@@ -0,0 +1,15 @@
+package net.mklew.hotelms.domain.guests;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/2/12
+ *        time 5:47 PM
+ */
+public class GuestHasNoWorkInformationException extends Exception
+{
+    public GuestHasNoWorkInformationException()
+    {
+    }
+
+
+}
