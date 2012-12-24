cc6e34ffa7c6e031b98d9c3991e9937fc9af1f35
Marek Lewandowski <mklew@github>
2012-12-24 19:58:02 +0100

    Add method to resolve DocumentType from string - option value from form.

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/DocumentType.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/DocumentType.java
@@ -7,5 +7,26 @@ package net.mklew.hotelms.domain.guests;
  */
 public enum DocumentType
 {
-    DRIVER_LICENSE, PASSPORT_ID, PERSONAL_ID
+    DRIVER_LICENSE, PASSPORT_ID, PERSONAL_ID, OTHER;
+
+    public static DocumentType fromString(String documentType)
+    {
+        if ("passport".equals(documentType))
+        {
+            return PASSPORT_ID;
+        } else if ("personalID".equals(documentType))
+        {
+            return PERSONAL_ID;
+        } else if ("driverLicense".equals(documentType))
+        {
+            return DRIVER_LICENSE;
+        } else if ("other".equals(documentType))
+        {
+            return OTHER;
+        } else
+        {
+            throw new RuntimeException("DocumentType from string: " + documentType + " cannot be resolved." +
+                    " Does not match supported types");
+        }
+    }
 }
