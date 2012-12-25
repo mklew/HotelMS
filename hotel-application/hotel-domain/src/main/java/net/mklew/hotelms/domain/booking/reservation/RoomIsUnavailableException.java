bdc0342b5a9cabd75d50c94575a099f220766fcf
Marek Lewandowski <mklew@github>
2012-12-25 22:00:05 +0100

    Add exception thrown when booking service fails to book reservation due to room unavailability

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/RoomIsUnavailableException.java
@@ -0,0 +1,11 @@
+package net.mklew.hotelms.domain.booking.reservation;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/25/12
+ *        time 9:12 PM
+ */
+public class RoomIsUnavailableException extends Throwable
+{
+
+}
