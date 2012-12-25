f65ef2ddbfc49de8aa34fb65517410b02ca65606
Marek Lewandowski <mklew@github>
2012-12-25 22:42:36 +0100

    Add equals and hashcode

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/Id.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/Id.java
@@ -1,7 +1,11 @@
 package net.mklew.hotelms.domain.booking;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 /**
  * Id class gives special meaning. Better than using String
+ *
  * @author: Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since: 9/9/12
  * time 8:32 PM
@@ -23,5 +27,34 @@ public class Id
     public Id concat(Id id)
     {
         return new Id(this.id + '.' + id.getId());
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (obj == this)
+        {
+            return true;
+        }
+        if (obj.getClass() != getClass())
+        {
+            return false;
+        }
+        Id rhs = (Id) obj;
+        return new EqualsBuilder()
+                .append(id, rhs.id)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(13, 47)
+                .append(id)
+                .toHashCode();
     }
 }
