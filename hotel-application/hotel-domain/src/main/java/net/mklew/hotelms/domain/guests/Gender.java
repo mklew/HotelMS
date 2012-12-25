74bd5d973c6121afdbe0d41195de68c4149cd2a5
Marek Lewandowski <mklew@github>
2012-12-25 20:02:56 +0100

    Add methods to evaluate Gender from string

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/Gender.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/guests/Gender.java
@@ -1,5 +1,8 @@
 package net.mklew.hotelms.domain.guests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 11/30/12
@@ -7,5 +10,26 @@ package net.mklew.hotelms.domain.guests;
  */
 public enum Gender
 {
-    MALE, FEMALE, OTHER
+    MALE, FEMALE, OTHER, REFUSED;
+
+    private static Map<String, Gender> namesToGender;
+
+    static
+    {
+        namesToGender = new HashMap<>(4);
+        namesToGender.put("male", MALE);
+        namesToGender.put("female", FEMALE);
+        namesToGender.put("other", OTHER);
+        namesToGender.put("refused", REFUSED);
+    }
+
+    public static Gender fromName(String name)
+    {
+        Gender gender = namesToGender.get(name);
+        if (gender == null)
+        {
+            throw new RuntimeException("Cannot resolve Gender from name: " + name);
+        }
+        return gender;
+    }
 }
