df89848e31739109a6704b6ff0efa37ac9c470ab
Marek Lewandowski <mklew@github>
2012-12-25 22:40:12 +0100

    Add equals and hashcode

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/ReservationId.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/ReservationId.java
@@ -2,6 +2,8 @@ package net.mklew.hotelms.domain.booking.reservation;
 
 
 import net.mklew.hotelms.domain.booking.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  * Identifies reservation
@@ -21,16 +23,50 @@ public abstract class ReservationId
 
     /**
      * Returns Id of reservation. This is either Id of single reservation or Id of group reservation.
+     *
      * @return Id of reservation
      */
     public abstract Id getId();
 
     /**
      * Returns group
+     *
      * @return Group if reservation belongs to group. Special case NoGroup otherwise
      */
     public Group getGroup()
     {
         return group;
     }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (obj == this)
+        {
+            return true;
+        }
+        if (obj.getClass() != getClass())
+        {
+            return false;
+        }
+        ReservationId rhs = (ReservationId) obj;
+        return new EqualsBuilder()
+                .append(group, rhs.group)
+                .append(getId(), rhs.getId())
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(13, 47)
+                .append(group)
+                .append(getId())
+                .toHashCode();
+    }
+
 }
