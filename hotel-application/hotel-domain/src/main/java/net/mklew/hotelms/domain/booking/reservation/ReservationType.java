8dab9b1da1f7018b6906d4cfb70f2f3347781fd2
Marek Lewandowski <mklew@github>
2012-12-25 21:01:15 +0100

    Add ReservationType enum.

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/ReservationType.java
@@ -0,0 +1,35 @@
+package net.mklew.hotelms.domain.booking.reservation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/25/12
+ *        time 8:57 PM
+ */
+public enum ReservationType
+{
+    SINGLE, GROUP, CORPORATE, AGENT;
+
+    private final static Map<String, ReservationType> nameToType;
+
+    static
+    {
+        nameToType = new HashMap<>(4);
+        nameToType.put("single", SINGLE);
+        nameToType.put("group", GROUP);
+        nameToType.put("corporate", CORPORATE);
+        nameToType.put("agent", AGENT);
+    }
+
+    public static ReservationType fromName(String name)
+    {
+        ReservationType reservationType = nameToType.get(name);
+        if (reservationType == null)
+        {
+            throw new RuntimeException("Cannot resolve from name " + name);
+        }
+        return reservationType;
+    }
+}
