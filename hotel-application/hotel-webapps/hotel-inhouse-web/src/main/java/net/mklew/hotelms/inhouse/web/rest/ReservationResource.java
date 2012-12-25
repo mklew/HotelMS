2a26b00b22c8a34d63ad6c6966ae349758f4bbee
Marek Lewandowski <mklew@github>
2012-12-25 20:19:24 +0100

    Begin implementing creation of new reservation which includes adding guest, booking etc.

--- hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/ReservationResource.java
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/ReservationResource.java
@@ -1,9 +1,16 @@
 package net.mklew.hotelms.inhouse.web.rest;
 
 import com.sun.jersey.spi.resource.Singleton;
+import net.mklew.hotelms.domain.booking.GuestRepository;
+import net.mklew.hotelms.domain.booking.reservation.ReservationFactory;
+import net.mklew.hotelms.domain.guests.Guest;
+import net.mklew.hotelms.domain.room.RoomName;
+import net.mklew.hotelms.domain.room.RoomRepository;
 import net.mklew.hotelms.inhouse.web.dto.GuestDto;
 import net.mklew.hotelms.inhouse.web.dto.MissingGuestInformation;
 import net.mklew.hotelms.inhouse.web.dto.ReservationDto;
+import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
+import org.hibernate.Session;
 import org.jcontainer.dna.Logger;
 
 import javax.ws.rs.*;
@@ -21,10 +28,19 @@ import java.util.Collection;
 public class ReservationResource
 {
     private final Logger logger;
+    private final ReservationFactory reservationFactory;
+    private final GuestRepository guestRepository;
+    private final HibernateSessionFactory hibernateSessionFactory;
+    private final RoomRepository roomRepository;
 
-    public ReservationResource(Logger logger)
+    public ReservationResource(Logger logger, ReservationFactory reservationFactory, GuestRepository guestRepository,
+                               HibernateSessionFactory hibernateSessionFactory, RoomRepository roomRepository)
     {
         this.logger = logger;
+        this.reservationFactory = reservationFactory;
+        this.guestRepository = guestRepository;
+        this.hibernateSessionFactory = hibernateSessionFactory;
+        this.roomRepository = roomRepository;
     }
 
     @GET
@@ -40,11 +56,48 @@ public class ReservationResource
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public ReservationDto createNewReservation(MultivaluedMap<String, String> formParams)
     {
-        logger.debug(formParams.toString());
+        Session session = hibernateSessionFactory.getCurrentSession();
+        session.beginTransaction();
+        logger.debug("Got new reservation with parameters: " + formParams.toString());
         try
         {
             GuestDto reservationOwner = GuestDto.fromReservationForm(formParams);
-        } catch (MissingGuestInformation missingGuestInformation)
+            ReservationDto reservationDto = ReservationDto.fromReservationForm(formParams);
+
+            Guest owner = null;
+            if (reservationOwner.exists())
+            {
+                owner = guestRepository.findGuestById(Long.parseLong(reservationOwner.id));
+            }
+            else
+            {
+                owner = new Guest(reservationOwner.socialTitle, reservationOwner.firstName,
+                        reservationOwner.surname, reservationOwner.gender, reservationOwner.idType,
+                        reservationOwner.idNumber, reservationOwner.phoneNumber);
+                owner.setPreferences(reservationOwner.preferences);
+                owner.setDateOfBirth(reservationOwner.dateOfBirthDate);
+                // owner.setEmailAddress(); // todo add field to form, dto, and set it here
+                // todo nationality, address and other
+                guestRepository.saveGuest(owner);
+            }
+
+            // get room
+            RoomName roomName = RoomName.getNameWithoutPrefix(reservationDto.getRoomName());
+            //roomRepository.getRoomByName(roomName);
+            // get rate
+
+            // create reservation using factory
+
+            // validate that reservation can be booked - room is available at that time
+
+            // persist reservation
+
+            // return persisted reservation dto
+
+            session.getTransaction().commit();
+            // return dto;
+        }
+        catch (MissingGuestInformation missingGuestInformation)
         {
             logger.error("Reservation owner has no sufficient information", missingGuestInformation);
             return null;
