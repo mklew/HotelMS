b3b27cf995e805bebd829a211a64abddac590ede
Marek Lewandowski <mklew@github>
2012-12-26 23:20:27 +0100

    Add simple save test just to check that mappings work.

---
+++ hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/ReservationPersistanceTest.java
@@ -0,0 +1,92 @@
+package net.mklew.hotelms.persistance;
+
+import net.mklew.hotelms.domain.booking.Id;
+import net.mklew.hotelms.domain.booking.ReservationStatus;
+import net.mklew.hotelms.domain.booking.reservation.Reservation;
+import net.mklew.hotelms.domain.booking.reservation.rates.RackRate;
+import net.mklew.hotelms.domain.guests.DocumentType;
+import net.mklew.hotelms.domain.guests.Gender;
+import net.mklew.hotelms.domain.guests.Guest;
+import net.mklew.hotelms.domain.room.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.joda.money.Money;
+import org.joda.time.DateMidnight;
+import org.joda.time.DateTime;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/26/12
+ *        time 9:57 PM
+ */
+public class ReservationPersistanceTest
+{
+    private SessionFactory sessionFactory;
+
+    @BeforeMethod
+    private void before() throws Exception
+    {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
+    @Test
+    public void should_save_reservation_and_retrieve_it() throws Exception
+    {
+        // given
+        final RoomName roomName = new RoomName("106");
+        final RoomType roomType = new RoomType("cheap");
+        final Money standardPrice = Money.parse("USD 100");
+        final Money upchargeExtraPerson = Money.parse("USD 50");
+        final Money upchargeExtraBed = Money.parse("USD 20");
+        final RackRate rackRate = new RackRate(standardPrice, upchargeExtraPerson, upchargeExtraBed, null);
+        final int maxExtraBeds = 2;
+        final Occupancy occupancy = new Occupancy(4, 2);
+        Room room = new Room("C", roomName, roomType, rackRate, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE,
+                maxExtraBeds, occupancy);
+        Guest guest1 = new Guest("Mr", "Johnny", "Doe", Gender.MALE, DocumentType.DRIVER_LICENSE, "123-321",
+                "555123456");
+
+        final DateTime checkIn = new DateTime(new DateMidnight(2012, 10, 5));
+        final DateTime checkOut = new DateTime(new DateMidnight(2012, 10, 10));
+        final int numberOfAdults = 2;
+        final int numberOfChildren = 0;
+        final int extraBeds = 0;
+
+        Reservation reservation = new Reservation(new Id(1), guest1,
+                rackRate, checkIn, checkOut, numberOfAdults, numberOfChildren, extraBeds, ReservationStatus.TECHNICAL);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(roomType);
+        session.save(room);
+        session.save(guest1);
+
+        session.getTransaction().commit();
+        session.close();
+
+        // when
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(reservation);
+
+        session.getTransaction().commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        final List<Reservation> list = session.createQuery("from Reservation ").list();
+        Reservation retrievedReservation = list.get(0);
+
+        assertThat(retrievedReservation.getNumberOfAdults()).isEqualTo(numberOfAdults);
+
+    }
+}
