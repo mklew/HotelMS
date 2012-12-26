6e9e300ede502fe020bd213b034b9e6ddc1d546b
Marek Lewandowski <mklew@github>
2012-12-26 21:49:05 +0100

    Add NightStatus enum

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/NightStatus.java
@@ -0,0 +1,13 @@
+package net.mklew.hotelms.domain.booking.reservation;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/26/12
+ *        time 9:03 PM
+ */
+public enum NightStatus
+{
+    USED,
+    NOT_USED,
+    EXTRA_NIGHT;
+}
