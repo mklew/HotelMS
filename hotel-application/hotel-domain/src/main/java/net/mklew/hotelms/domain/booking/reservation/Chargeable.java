4b3dee2a1edc9dcd183b4a13c8138fc252eeef82
Marek Lewandowski <mklew@github>
2012-12-26 23:18:34 +0100

    Implement serializable

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/Chargeable.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/Chargeable.java
@@ -2,12 +2,14 @@ package net.mklew.hotelms.domain.booking.reservation;
 
 import org.joda.money.Money;
 
+import java.io.Serializable;
+
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 12/26/12
  *        time 9:15 PM
  */
-public interface Chargeable
+public interface Chargeable extends Serializable
 {
     Money charge();
 }
