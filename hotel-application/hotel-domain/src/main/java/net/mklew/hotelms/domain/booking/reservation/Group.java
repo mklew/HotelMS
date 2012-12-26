b27220073c084d6f9abace6ace5754a24bb38dca
Marek Lewandowski <mklew@github>
2012-12-26 20:18:01 +0100

    Change Group class completely. It has simple Id and Set of child objects.
    Add method to add new reservation to group.

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/Group.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/Group.java
@@ -2,17 +2,59 @@ package net.mklew.hotelms.domain.booking.reservation;
 
 import net.mklew.hotelms.domain.booking.Id;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
- * @author: Marek Lewandowski <marek.m.lewandowski@gmail.com>
- * @since: 9/9/12
- * time 8:27 PM
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 9/9/12
+ *        time 8:27 PM
  */
 public class Group
 {
-    public final static Group NO_GROUP = new NoGroup();
+    private Id groupId;
+    private Set<GroupReservation> reservationsInGroup;
 
-    public Id getId()
+    Group()
     {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public synchronized void addReservationToGroup(Reservation reservationToAdd)
+    {
+        long nextSequence = getNextSequenceNumber();
+        GroupReservation groupReservation = new GroupReservation(this, nextSequence, reservationToAdd);
+        reservationsInGroup.add(groupReservation);
+    }
+
+    private long getNextSequenceNumber()
+    {
+        if (reservationsInGroup.size() == 0)
+        {
+            return 0;
+        }
+        SortedSet<Long> sequences = new TreeSet<>();
+        for (GroupReservation groupReservation : reservationsInGroup)
+        {
+            sequences.add(groupReservation.getSequence());
+        }
+        return sequences.last() + 1;
+    }
+
+    public Group(Id groupId)
+    {
+        this.groupId = groupId;
+        reservationsInGroup = new TreeSet<>();
+    }
+
+    public Id getGroupId()
+    {
+        return groupId;
+    }
+
+    public Set<GroupReservation> getReservationsInGroup()
+    {
+        return Collections.unmodifiableSet(reservationsInGroup);
     }
 }
