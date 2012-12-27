cccdf479d4dd047239cebccdde7ea039eede93e6
Marek Lewandowski <mklew@github>
2012-12-27 15:41:32 +0100

    Bootstrap some reservations

--- hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/hibernate/configuration/DbBootstrap.java
+++ hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/hibernate/configuration/DbBootstrap.java
@@ -1,5 +1,8 @@
 package net.mklew.hotelms.persistance.hibernate.configuration;
 
+import net.mklew.hotelms.domain.booking.Id;
+import net.mklew.hotelms.domain.booking.ReservationStatus;
+import net.mklew.hotelms.domain.booking.reservation.Reservation;
 import net.mklew.hotelms.domain.booking.reservation.rates.*;
 import net.mklew.hotelms.domain.guests.DocumentType;
 import net.mklew.hotelms.domain.guests.Gender;
@@ -8,6 +11,7 @@ import net.mklew.hotelms.domain.room.*;
 import org.hibernate.Session;
 import org.jcontainer.dna.Logger;
 import org.joda.money.Money;
+import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
 import org.picocontainer.Startable;
 
@@ -61,55 +65,109 @@ public class DbBootstrap implements Startable
         Money upchargeExtraPerson = Money.parse("USD 110");
         Money upchargeExtraBed = Money.parse("USD 120");
         RackRate rackRate = new RackRate(standardPrice, upchargeExtraPerson, upchargeExtraBed, null);
-        RackRate rackRate1 = new RackRate(standardPrice.plus(10), upchargeExtraPerson.plus(10), upchargeExtraBed.plus(10), null);
-        RackRate rackRate2 = new RackRate(standardPrice.plus(20), upchargeExtraPerson.plus(20), upchargeExtraBed.plus(20), null);
-        RackRate rackRate3 = new RackRate(standardPrice.plus(30), upchargeExtraPerson.plus(30), upchargeExtraBed.plus(30), null);
-        RackRate rackRate4 = new RackRate(standardPrice.plus(40), upchargeExtraPerson.plus(40), upchargeExtraBed.plus(40), null);
-        RackRate rackRate5 = new RackRate(standardPrice.plus(50), upchargeExtraPerson.plus(50), upchargeExtraBed.plus(50), null);
+        RackRate rackRate1 = new RackRate(standardPrice.plus(10), upchargeExtraPerson.plus(10),
+                upchargeExtraBed.plus(10), null);
+        RackRate rackRate2 = new RackRate(standardPrice.plus(20), upchargeExtraPerson.plus(20),
+                upchargeExtraBed.plus(20), null);
+        RackRate rackRate3 = new RackRate(standardPrice.plus(30), upchargeExtraPerson.plus(30),
+                upchargeExtraBed.plus(30), null);
+        RackRate rackRate4 = new RackRate(standardPrice.plus(40), upchargeExtraPerson.plus(40),
+                upchargeExtraBed.plus(40), null);
+        RackRate rackRate5 = new RackRate(standardPrice.plus(50), upchargeExtraPerson.plus(50),
+                upchargeExtraBed.plus(50), null);
 
-        Room L100 = new Room("L", new RoomName("100"), luxury, rackRate, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 1, new Occupancy(3, 4));
-        Room L101 = new Room("L", new RoomName("101"), luxury, rackRate1, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 1, new Occupancy(2, 3));
-        Room L102 = new Room("L", new RoomName("102"), luxury, rackRate2, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 3, new Occupancy(2, 4));
-        Room C103 = new Room("C", new RoomName("103"), cheap, rackRate3, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 4, new Occupancy(4, 10));
-        Room C104 = new Room("C", new RoomName("104"), cheap, rackRate4, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 5, new Occupancy(6, 12));
-        Room N105 = new Room("N", new RoomName("105"), niceOne, rackRate5, HousekeepingStatus.CLEAN, RoomAvailability.AVAILABLE, 2, new Occupancy(2, 5));
+        Room L100 = new Room("L", new RoomName("100"), luxury, rackRate, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 1, new Occupancy(3, 4));
+        Room L101 = new Room("L", new RoomName("101"), luxury, rackRate1, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 1, new Occupancy(2, 3));
+        Room L102 = new Room("L", new RoomName("102"), luxury, rackRate2, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 3, new Occupancy(2, 4));
+        Room C103 = new Room("C", new RoomName("103"), cheap, rackRate3, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 4, new Occupancy(4, 10));
+        Room C104 = new Room("C", new RoomName("104"), cheap, rackRate4, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 5, new Occupancy(6, 12));
+        Room N105 = new Room("N", new RoomName("105"), niceOne, rackRate5, HousekeepingStatus.CLEAN,
+                RoomAvailability.AVAILABLE, 2, new Occupancy(2, 5));
 
         rooms = Arrays.asList(L100, L101, L102, C103, C104, N105);
 
         Collection<Rate> rates;
 
-        Season season = new BasicSeason("winter special", new AvailabilityPeriod(DateTime.now(), DateTime.now().plusDays(90), true));
-        Season season2 = new BasicSeason("christmas special", new AvailabilityPeriod(DateTime.now(), DateTime.now().plusDays(30), true));
+        Season season = new BasicSeason("winter special", new AvailabilityPeriod(DateTime.now(),
+                DateTime.now().plusDays(90), true));
+        Season season2 = new BasicSeason("christmas special", new AvailabilityPeriod(DateTime.now(),
+                DateTime.now().plusDays(30), true));
 
-        Rate rate1_L100 = new SeasonRate(Money.parse("USD 50"), Money.parse("USD 60"), Money.parse("USD 70"), L100, season);
-        Rate rate2_L100 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 60"), Money.parse("USD 70"), L100, season2);
-        Rate rate1_L101 = new SeasonRate(Money.parse("USD 60"), Money.parse("USD 70"), Money.parse("USD 60"), L101, season);
-        Rate rate2_L101 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 70"), Money.parse("USD 60"), L101, season2);
-        Rate rate1_L102 = new SeasonRate(Money.parse("USD 60"), Money.parse("USD 70"), Money.parse("USD 60"), L102, season);
-        Rate rate2_L102 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 70"), Money.parse("USD 60"), L102, season2);
-        Rate rate1_C103 = new SeasonRate(Money.parse("USD 40"), Money.parse("USD 70"), Money.parse("USD 60"), C103, season);
-        Rate rate2_C103 = new SeasonRate(Money.parse("USD 30"), Money.parse("USD 70"), Money.parse("USD 60"), C103, season2);
-        Rate rate1_C104 = new SeasonRate(Money.parse("USD 70"), Money.parse("USD 70"), Money.parse("USD 60"), C104, season);
-        Rate rate2_C104 = new SeasonRate(Money.parse("USD 50"), Money.parse("USD 70"), Money.parse("USD 60"), C104, season2);
-        Rate rate1_N105 = new SeasonRate(Money.parse("USD 80"), Money.parse("USD 70"), Money.parse("USD 60"), N105, season);
-        Rate rate2_N105 = new SeasonRate(Money.parse("USD 90"), Money.parse("USD 70"), Money.parse("USD 60"), N105, season2);
+        Rate rate1_L100 = new SeasonRate(Money.parse("USD 50"), Money.parse("USD 60"), Money.parse("USD 70"), L100,
+                season);
+        Rate rate2_L100 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 60"), Money.parse("USD 70"), L100,
+                season2);
+        Rate rate1_L101 = new SeasonRate(Money.parse("USD 60"), Money.parse("USD 70"), Money.parse("USD 60"), L101,
+                season);
+        Rate rate2_L101 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 70"), Money.parse("USD 60"), L101,
+                season2);
+        Rate rate1_L102 = new SeasonRate(Money.parse("USD 60"), Money.parse("USD 70"), Money.parse("USD 60"), L102,
+                season);
+        Rate rate2_L102 = new SeasonRate(Money.parse("USD 20"), Money.parse("USD 70"), Money.parse("USD 60"), L102,
+                season2);
+        Rate rate1_C103 = new SeasonRate(Money.parse("USD 40"), Money.parse("USD 70"), Money.parse("USD 60"), C103,
+                season);
+        Rate rate2_C103 = new SeasonRate(Money.parse("USD 30"), Money.parse("USD 70"), Money.parse("USD 60"), C103,
+                season2);
+        Rate rate1_C104 = new SeasonRate(Money.parse("USD 70"), Money.parse("USD 70"), Money.parse("USD 60"), C104,
+                season);
+        Rate rate2_C104 = new SeasonRate(Money.parse("USD 50"), Money.parse("USD 70"), Money.parse("USD 60"), C104,
+                season2);
+        Rate rate1_N105 = new SeasonRate(Money.parse("USD 80"), Money.parse("USD 70"), Money.parse("USD 60"), N105,
+                season);
+        Rate rate2_N105 = new SeasonRate(Money.parse("USD 90"), Money.parse("USD 70"), Money.parse("USD 60"), N105,
+                season2);
 
         Collection<Season> seasons = Arrays.asList(season, season2);
         rates = Arrays.asList(rate1_L100, rate2_L100, rate1_L101, rate2_L101, rate1_L102, rate2_L102, rate1_C103,
                 rate2_C103, rate1_C104, rate2_C104, rate1_N105, rate2_N105);
 
 
-        Guest guest1 = new Guest("Mr", "John", "Doe", Gender.MALE, DocumentType.DRIVER_LICENSE, "Drivers123", "555123123");
-        Guest guest2 = new Guest("Mr", "Johnson", "Donnel", Gender.MALE, DocumentType.DRIVER_LICENSE, "DRI123", "555555123");
-        Guest guest3 = new Guest("Mr", "Johnathan", "Doougles", Gender.MALE, DocumentType.DRIVER_LICENSE, "DDRI1555", "555123123");
-        Guest guest4 = new Guest("Miss", "Joana", "Dooues", Gender.FEMALE, DocumentType.PERSONAL_ID, "APK132555", "819238923");
-        Guest guest5 = new Guest("Ms", "Kate", "Hudson", Gender.FEMALE, DocumentType.PERSONAL_ID, "DSA123889", "534098123");
+        Guest guest1 = new Guest("Mr", "John", "Doe", Gender.MALE, DocumentType.DRIVER_LICENSE, "Drivers123",
+                "555123123");
+        Guest guest2 = new Guest("Mr", "Johnson", "Donnel", Gender.MALE, DocumentType.DRIVER_LICENSE, "DRI123",
+                "555555123");
+        Guest guest3 = new Guest("Mr", "Johnathan", "Doougles", Gender.MALE, DocumentType.DRIVER_LICENSE, "DDRI1555",
+                "555123123");
+        Guest guest4 = new Guest("Miss", "Joana", "Dooues", Gender.FEMALE, DocumentType.PERSONAL_ID, "APK132555",
+                "819238923");
+        Guest guest5 = new Guest("Ms", "Kate", "Hudson", Gender.FEMALE, DocumentType.PERSONAL_ID, "DSA123889",
+                "534098123");
         Guest guest6 = new Guest("Mr", "Jack", "Hack", Gender.MALE, DocumentType.PERSONAL_ID, "LKK123555", "123589124");
-        Guest guest7 = new Guest("Ms", "Ewa", "Kowalska", Gender.FEMALE, DocumentType.PERSONAL_ID, "PAS123553", "123985332");
-        Guest guest8 = new Guest("Ms", "Karolina", "Iksinska", Gender.FEMALE, DocumentType.DRIVER_LICENSE, "DRI132511", "898123532");
-        Guest guest9 = new Guest("Mr", "Grzegorz", "Brzeczyszczykiewicz", Gender.MALE, DocumentType.PERSONAL_ID, "AAA123123", "342089123");
-        Guest guest10 = new Guest("Mrs", "John", "Smith", Gender.MALE, DocumentType.DRIVER_LICENSE, "DRI132135", "12312353");
-        Collection<Guest> guests = Arrays.asList(guest1, guest2, guest3, guest4, guest5, guest6, guest7, guest8, guest9, guest10);
+        Guest guest7 = new Guest("Ms", "Ewa", "Kowalska", Gender.FEMALE, DocumentType.PERSONAL_ID, "PAS123553",
+                "123985332");
+        Guest guest8 = new Guest("Ms", "Karolina", "Iksinska", Gender.FEMALE, DocumentType.DRIVER_LICENSE,
+                "DRI132511", "898123532");
+        Guest guest9 = new Guest("Mr", "Grzegorz", "Brzeczyszczykiewicz", Gender.MALE, DocumentType.PERSONAL_ID,
+                "AAA123123", "342089123");
+        Guest guest10 = new Guest("Mrs", "John", "Smith", Gender.MALE, DocumentType.DRIVER_LICENSE, "DRI132135",
+                "12312353");
+        Collection<Guest> guests = Arrays.asList(guest1, guest2, guest3, guest4, guest5, guest6, guest7, guest8,
+                guest9, guest10);
+
+        Reservation reservation = new Reservation(Id.NO_ID, guest1, rackRate, new DateTime(new DateMidnight(2012, 12,
+                1)),
+                new DateTime(new DateMidnight(2012, 12, 5)), 2, 0, 0, ReservationStatus.RESERVED);
+        Reservation reservation2 = new Reservation(Id.NO_ID, guest2, rackRate2, new DateTime(new DateMidnight(2012,
+                12, 3)),
+                new DateTime(new DateMidnight(2012, 12, 8)), 2, 0, 0, ReservationStatus.RESERVED);
+        Reservation reservation3 = new Reservation(Id.NO_ID, guest2, rate1_C103, new DateTime(new DateMidnight(2012,
+                12, 2)),
+                new DateTime(new DateMidnight(2012, 12, 15)), 3, 0, 0, ReservationStatus.RESERVED);
+        Reservation reservation4 = new Reservation(Id.NO_ID, guest3, rate1_N105, new DateTime(new DateMidnight(2012,
+                12, 10)),
+                new DateTime(new DateMidnight(2012, 12, 14)), 2, 0, 0, ReservationStatus.RESERVED);
+        Reservation reservation5 = new Reservation(Id.NO_ID, guest5, rate2_C104, new DateTime(new DateMidnight(2012,
+                12, 20)),
+                new DateTime(new DateMidnight(2012, 12, 28)), 1, 0, 0, ReservationStatus.RESERVED);
+        Collection<Reservation> reservations = Arrays.asList(reservation, reservation2, reservation3, reservation4,
+                reservation5);
+
         // bootstrapping data
         session.beginTransaction();
         logger.debug("adding room types:");
@@ -144,6 +202,13 @@ public class DbBootstrap implements Startable
         {
             logger.debug("guest: " + guest.toString());
             session.save(guest);
+        }
+
+        logger.debug("adding reservations");
+        for (Reservation res : reservations)
+        {
+            logger.debug("reservation: " + res.toString());
+            session.save(res);
         }
 
         session.getTransaction().commit();
