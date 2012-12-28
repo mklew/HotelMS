90851e7d233a0640bac2106ed8e6f1d4f38fa5c6
Marek Lewandowski <mklew@github>
2012-12-28 14:21:18 +0100

    Change implementation just to return rates from room. This method might be refactored later

--- hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/RateRepositoryHibernate.java
+++ hotel-application/hotel-persistance/src/main/java/net/mklew/hotelms/persistance/RateRepositoryHibernate.java
@@ -4,10 +4,7 @@ import net.mklew.hotelms.domain.booking.reservation.rates.Rate;
 import net.mklew.hotelms.domain.booking.reservation.rates.RateRepository;
 import net.mklew.hotelms.domain.room.Room;
 import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
-import org.hibernate.Query;
-import org.hibernate.Session;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -15,7 +12,7 @@ import java.util.Collection;
  * @since 11/3/12
  *        Time: 9:14 PM
  */
-public class RateRepositoryHibernate  extends HibernateRepository implements RateRepository
+public class RateRepositoryHibernate extends HibernateRepository implements RateRepository
 {
     private final static String ALL_RATES_FOR_ROOM = "select rate from Rate rate where rate.room = :room";
 
@@ -27,14 +24,6 @@ public class RateRepositoryHibernate  extends HibernateRepository implements Rat
     @Override
     public Collection<Rate> getAllRatesForRoom(Room room)
     {
-        final Session session = getCurrentSession();
-        final Query query = session.createQuery(ALL_RATES_FOR_ROOM).setParameter("room", room);
-        Collection<Rate> rates = query.list();
-        if(rates == null)
-        {
-            rates = new ArrayList<>();
-        }
-        rates.add(room.rackRate()); // rack rates are not mapped as rate entity so rack rate need to be added separately
-        return rates;
+        return room.getRates();
     }
 }
