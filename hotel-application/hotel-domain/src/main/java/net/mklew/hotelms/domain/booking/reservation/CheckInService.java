ecd4c7ffdbc9b54c97aa646274b628f2bb69fca5
Marek Lewandowski <mklew@github>
2012-12-28 22:25:15 +0100

    Add CheckInService. Class for business logic about checking in reservation

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/CheckInService.java
@@ -0,0 +1,19 @@
+package net.mklew.hotelms.domain.booking.reservation;
+
+/**
+ * CheckIn process might involve some kind of business logic. This is place for it.
+ * <p/>
+ * Current implementation has no business logic.
+ *
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/28/12
+ *        time 10:11 PM
+ */
+public class CheckInService
+{
+
+    public void checkIn(Reservation reservation)
+    {
+        reservation.checkIn();
+    }
+}
