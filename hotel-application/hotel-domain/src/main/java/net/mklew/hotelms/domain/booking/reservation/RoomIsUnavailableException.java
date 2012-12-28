3f320b77341a51dddca6afe6806ef907a414311d
Marek Lewandowski <mklew@github>
2012-12-28 16:24:48 +0100

    Add Data to exception

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/RoomIsUnavailableException.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/RoomIsUnavailableException.java
@@ -1,5 +1,7 @@
 package net.mklew.hotelms.domain.booking.reservation;
 
+import org.joda.time.DateTime;
+
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
  * @since 12/25/12
@@ -7,5 +9,29 @@ package net.mklew.hotelms.domain.booking.reservation;
  */
 public class RoomIsUnavailableException extends Throwable
 {
+    private String roomName;
+    private DateTime checkIn;
+    private DateTime checkOut;
 
+    public RoomIsUnavailableException(String roomName, DateTime checkIn, DateTime checkOut)
+    {
+        this.roomName = roomName;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public String getRoomName()
+    {
+        return roomName;
+    }
+
+    public DateTime getCheckIn()
+    {
+        return checkIn;
+    }
+
+    public DateTime getCheckOut()
+    {
+        return checkOut;
+    }
 }
