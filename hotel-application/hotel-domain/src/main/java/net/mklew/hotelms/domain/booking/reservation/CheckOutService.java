d54c10d0dd0e1ad767356256345bb8f36eb11761
Marek Lewandowski <mklew@github>
2012-12-28 22:25:48 +0100

    Add CheckOut service. Place for business logic involving checking out reservation.

---
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/CheckOutService.java
@@ -0,0 +1,21 @@
+package net.mklew.hotelms.domain.booking.reservation;
+
+/**
+ * CheckingOut reservation might involve complex business logic.
+ * <p/>
+ * Example of business rule:
+ * check if its not early checkout and do it right according to policy about early checkouts,
+ * <p/>
+ * This is place for it.
+ *
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/28/12
+ *        time 10:14 PM
+ */
+public class CheckOutService
+{
+    public void checkOut(Reservation reservation)
+    {
+        reservation.checkOut();
+    }
+}
