2ec59ac161628fde41daddc561fdc8bc334f148c
Marek Lewandowski <mklew@github>
2012-12-31 19:57:22 +0100

    Add Login Resource / endpoint. It's needed for making authentication request but all job is done by Shiro's filter.

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/LoginResource.java
@@ -0,0 +1,27 @@
+package net.mklew.hotelms.inhouse.web.rest;
+
+import com.sun.jersey.spi.resource.Singleton;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 12/31/12
+ *        time 3:12 PM
+ */
+@Path("login")
+@Singleton
+public class LoginResource
+{
+    /**
+     * This is only something like ping or loopback. Credentials are checked using BasicHttpAuthenticationFilter by
+     * Shiro
+     */
+    @POST
+    public Response login()
+    {
+        return Response.ok().build();
+    }
+}
