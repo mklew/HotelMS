ae35c3976b636961b1e6c622795c7210aac76d5b
Marek Lewandowski <mklew@github>
2013-01-01 02:09:36 +0100

    Add PermResource - service which allows to check if current subject has given permission.

---
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/rest/PermResource.java
@@ -0,0 +1,36 @@
+package net.mklew.hotelms.inhouse.web.rest;
+
+import com.sun.jersey.spi.resource.Singleton;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 1/1/13
+ *        time 1:48 AM
+ */
+@Singleton
+@Path("perm")
+public class PermResource
+{
+
+    @GET
+    public Response hasPerm(@QueryParam("perm") String permission)
+    {
+        final Subject subject = SecurityUtils.getSubject();
+        final boolean permitted = subject.isPermitted(permission);
+        if (permitted)
+        {
+            return Response.ok().build();
+        }
+        else
+        {
+            return Response.ok().status(403).build();
+        }
+    }
+}
