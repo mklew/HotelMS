02faa2025cfbd1a9070e0d23051719e281ce7aab
Marek Lewandowski <mklew@github>
2013-01-01 21:26:22 +0100

    Add service methods to change Room / Rate / checkIn date / checkOut date

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/BookingService.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/BookingService.java
@@ -1,6 +1,14 @@
 package net.mklew.hotelms.domain.booking.reservation;
 
+import net.mklew.hotelms.domain.booking.reservation.rates.Rate;
+import net.mklew.hotelms.domain.room.Room;
+import net.mklew.hotelms.domain.room.RoomName;
+import net.mklew.hotelms.domain.room.RoomNotFoundException;
 import net.mklew.hotelms.domain.room.RoomRepository;
+import org.joda.time.DateTime;
+
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * Implements booking business logic. What's below is very simple business logic with no overbooking taken into account
@@ -26,6 +34,7 @@ public class BookingService
                 reservation.getCheckOut());
     }
 
+
     public void bookReservation(Reservation reservation) throws RoomIsUnavailableException
     {
         if (isRoomAvailable(reservation))
@@ -39,4 +48,84 @@ public class BookingService
                     reservation.getCheckOut());
         }
     }
+
+    /**
+     * Changes room, using default rate (Rack rate) for given reservation
+     *
+     * @param reservation
+     * @param room
+     */
+    private void changeRoom(Reservation reservation, Room room)
+    {
+        reservation.changeRoom(room, room.rackRate());
+    }
+
+    /**
+     * Changes rate for given reservation. Rate must be available in given room
+     *
+     * @param reservation
+     * @param chosenRate
+     */
+    public void changeRate(Reservation reservation, String chosenRate) throws RateNotFoundException
+    {
+        final Set<Rate> rates = reservation.getRoom().getRates();
+        Rate rate = getChosenRate(chosenRate, rates);
+        if (rates.contains(chosenRate))
+        {
+            reservation.changeRate(rate);
+        }
+        else
+        {
+            throw new RateNotFoundException(rate);
+        }
+    }
+
+    private static Rate getChosenRate(String rateName, Collection<Rate> rates)
+    {
+        for (Rate rate : rates)
+        {
+            if (rate.getRateName().equals(rateName))
+            {
+                return rate;
+            }
+        }
+        throw new RuntimeException("Rate not found");
+    }
+
+    public void rebookReservationRoom(Reservation reservation, RoomName roomName, String newRate,
+                                      DateTime newCheckIn, DateTime
+            newCheckOut) throws RoomNotFoundException, RateNotFoundException, RoomIsUnavailableException
+    {
+        final Room room = roomRepository.getRoomByName(roomName);
+        changeRoom(reservation, room);
+        changeRate(reservation, newRate);
+        changeCheckInAndCheckOut(reservation, newCheckIn, newCheckOut);
+    }
+
+    private void changeCheckInAndCheckOut(Reservation reservation, DateTime newCheckIn,
+                                          DateTime newCheckOut) throws RoomIsUnavailableException
+
+    {
+        validateThatRoomIsAvailable(reservation, newCheckIn, newCheckOut);
+        reservation.changeCheckInAndCheckOut(newCheckIn, newCheckOut);
+    }
+
+    private void validateThatRoomIsAvailable(Reservation reservation, DateTime newCheckIn,
+                                             DateTime newCheckOut) throws RoomIsUnavailableException
+    {
+        if (!(roomRepository.isRoomAvailableBetweenDates(reservation.getRoom(), newCheckIn, newCheckOut)))
+        {
+            throw new RoomIsUnavailableException(reservation.getRoom().fullRoomName(), newCheckIn,
+                    newCheckOut);
+        }
+    }
+
+    public void rebookVisit(Reservation reservation, DateTime newCheckIn,
+                            DateTime newCheckOut) throws RoomIsUnavailableException
+    {
+        validateThatRoomIsAvailable(reservation, newCheckIn, newCheckOut);
+        reservation.changeCheckInAndCheckOut(newCheckIn, newCheckOut);
+    }
+
+
 }
