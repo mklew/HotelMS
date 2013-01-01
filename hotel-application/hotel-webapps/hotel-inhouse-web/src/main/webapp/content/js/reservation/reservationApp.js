2f162f14bfb5c6b1d94567814b8a1da5dbb63e57
Marek Lewandowski <mklew@github>
2013-01-01 21:32:00 +0100

    Add methods and route for Update view

--- hotel-application/hotel-webapps/hotel-inhouse-web/src/main/webapp/content/js/reservation/reservationApp.js
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/webapp/content/js/reservation/reservationApp.js
@@ -2,7 +2,7 @@ angular.module('ReservationApp', ['reservationResource']).
 config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ReservationListCtrl, templateUrl:'list.html'}).
-      // when('/edit/:reservationId', {controller:ReservationEditCtrl, templateUrl:'detail.html'}).
+      when('/edit/:reservationId', {controller:ReservationEditCtrl, templateUrl:'detail.html'}).
        // when('/new', {controller:ReservationCreateCtrl, templateUrl:'detail.html'}).
       otherwise({redirectTo:'/'});
   });
@@ -71,14 +71,54 @@ function ReservationCreateCtrl($scope, $location, Reservation) {
     }
 }
 
-function ReservationEditCtrl($scope, $routeParams, $location, Reservation)
+function ReservationEditCtrl($scope, $routeParams, $location, $http, Reservation)
 {
   var self = this;
+  $scope.chargesCalculated = false;
+
+  $scope.calculateCharges = function()
+  {
+    var room = getCurrentRoomObject()
+    $http.get('/rest/charge/room/' + room.number + '/rate/' + escape($scope.reservation.rateType), {
+      params : {"checkin" : $scope.reservation.checkin,
+      "checkout" : $scope.reservation.checkout}
+    }).success(function(data){
+      $scope.amount = data.amount;
+      $scope.chargesCalculated =  true;
+    }); 
+  }
+
+  function getCurrentRoomObject()
+  {   
+    for(var room in $scope.rooms)
+    {
+      if($scope.rooms[room].id === $scope.reservation.roomName)
+      {
+        return $scope.rooms[room];
+      }
+    }
+  }
+
+  $scope.extraBeds = [{ extraBed: 0 },{extraBed: 1},{extraBed:2}, {extraBed:3},
+                 { extraBed: 4 },{extraBed: 5},{extraBed:6}, {extraBed:7},
+                 { extraBed: 8 },{extraBed: 9},{extraBed:10}, {extraBed:11}
+                ]
 
   Reservation.get({ id: $routeParams.reservationId }, function(reservation){
+     reservation.numberOfAdults = parseInt(reservation.numberOfAdults);
+     reservation.numberOfChildren = parseInt(reservation.numberOfChildren);
+     reservation.roomExtraBed = parseInt(reservation.roomExtraBed);
      self.original = reservation;
     $scope.original = reservation;
     $scope.reservation = new Reservation(self.original);
+  });
+
+  $http.get("/rest/room/types").success(function(data, status, headers, config){
+    $scope.types = data;
+  });
+
+  $http.get("/rest/room").success(function(data, status, headers, config){
+    $scope.rooms = data;
   });
 
   $scope.isClean = function() {
