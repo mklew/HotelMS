a08c8d2f8f132d28ebd88e7e23c42238e4aa22e5
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-05-27 00:40:51 +0200

    Add SheetData DTO

--- hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/dto/SheetData.java
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/dto/SheetData.java
@@ -1,9 +1,40 @@
 package net.mklew.hotelms.inhouse.web.dto;
 
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * @author Marek Lewandowski
  * @since 5/24/13
  */
 public class SheetData
 {
+    private String roomName;
+
+    private Collection<ReservationSheetData> reservations = Collections.emptyList();
+
+
+    public SheetData()
+    {
+    }
+
+    public String getRoomName()
+    {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName)
+    {
+        this.roomName = roomName;
+    }
+
+    public Collection<ReservationSheetData> getReservations()
+    {
+        return reservations;
+    }
+
+    public void setReservations(Collection<ReservationSheetData> reservations)
+    {
+        this.reservations = reservations;
+    }
 }
