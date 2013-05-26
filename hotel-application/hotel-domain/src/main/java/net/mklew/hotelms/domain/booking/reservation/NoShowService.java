8304775e0eb55a16cb43d1193eb17ca628019e9a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-05-27 00:39:09 +0200

    Add no show service

--- hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/NoShowService.java
+++ hotel-application/hotel-domain/src/main/java/net/mklew/hotelms/domain/booking/reservation/NoShowService.java
@@ -1,9 +1,12 @@
 package net.mklew.hotelms.domain.booking.reservation;
 
 /**
+ * Marks reservation as no show and performs business logic related to no shows.
+ *
  * @author Marek Lewandowski
  * @since 5/26/13
  */
 public interface NoShowService
 {
+    public void markAsNoShow(Reservation reservation);
 }
