bbfaf1535159f8872c6d82ce7372e26e9a7be024
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-05-27 00:47:59 +0200

    Audit results

--- hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/dto/AuditResults.java
+++ hotel-application/hotel-webapps/hotel-inhouse-web/src/main/java/net/mklew/hotelms/inhouse/web/dto/AuditResults.java
@@ -1,9 +1,33 @@
 package net.mklew.hotelms.inhouse.web.dto;
 
+import java.util.Collection;
+
 /**
  * @author Marek Lewandowski
  * @since 5/26/13
  */
 public class AuditResults
 {
+    private Collection<ReservationDto> noShows;
+    private Collection<ReservationDto> tommorrowCheckIns;
+
+    public void setNoShows(Collection<ReservationDto> noShows)
+    {
+        this.noShows = noShows;
+    }
+
+    public Collection<ReservationDto> getNoShows()
+    {
+        return noShows;
+    }
+
+    public void setTommorrowCheckIns(Collection<ReservationDto> tommorrowCheckIns)
+    {
+        this.tommorrowCheckIns = tommorrowCheckIns;
+    }
+
+    public Collection<ReservationDto> getTommorrowCheckIns()
+    {
+        return tommorrowCheckIns;
+    }
 }
