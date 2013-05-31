10b77dcca67622feda46ddccfdf63c312efce840
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-05-31 15:53:45 +0200

    Run different in memory database for each of tests. This is really brute solution but works fine

--- hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/IntegrationTest.java
+++ hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/IntegrationTest.java
@@ -1,12 +1,14 @@
 package net.mklew.hotelms.persistance;
 
 import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
-import net.mklew.hotelms.persistance.hibernate.configuration.NativelyConfiguredHibernateSessionFactory;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.jcontainer.dna.Logger;
-import org.testng.annotations.AfterGroups;
-import org.testng.annotations.BeforeGroups;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.mockito.Mockito.mock;
 
@@ -15,23 +17,30 @@ import static org.mockito.Mockito.mock;
  * @since 12/27/12
  *        time 8:11 PM
  */
-@Test(groups = {"integration"})
+@Test
 public class IntegrationTest
 {
     protected SessionFactory sessionFactory;
     protected HibernateSessionFactory hibernateSessionFactory;
+    private final static AtomicInteger seq = new AtomicInteger();
 
-    @BeforeGroups(groups = {"integration"})
+    @BeforeMethod
     public void setUpHibernate() throws Exception
     {
-        Logger logger = mock(Logger.class);
-        NativelyConfiguredHibernateSessionFactory hibernateSessionFactory = new
-                NativelyConfiguredHibernateSessionFactory(logger);
-        this.sessionFactory = hibernateSessionFactory.getSessionFactory();
+        Configuration configuration = new Configuration().configure();
+        final String connection = "jdbc:h2:mem:db" + seq.incrementAndGet() + ";DB_CLOSE_DELAY=0;MVCC=true";
+        configuration.setProperty("hibernate.connection.url", connection);
+        ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
+        serviceRegistryBuilder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = serviceRegistryBuilder.buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        final StubHibernateSessionFactory hibernateSessionFactory = new StubHibernateSessionFactory(sessionFactory);
         this.hibernateSessionFactory = hibernateSessionFactory;
+        this.sessionFactory = hibernateSessionFactory.getSessionFactory();
     }
 
-    @AfterGroups(groups = {"integration"})
+    @AfterMethod
     public void putItDown() throws Exception
     {
         sessionFactory.close();
