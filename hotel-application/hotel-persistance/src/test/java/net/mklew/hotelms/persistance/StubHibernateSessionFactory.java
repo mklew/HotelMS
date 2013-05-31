dcab1162d50b1d58f6d99723787c79fd34d97f2d
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-05-31 15:52:30 +0200

    move to src/test

---
+++ hotel-application/hotel-persistance/src/test/java/net/mklew/hotelms/persistance/StubHibernateSessionFactory.java
@@ -0,0 +1,42 @@
+package net.mklew.hotelms.persistance;
+
+import net.mklew.hotelms.persistance.hibernate.configuration.HibernateSessionFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
+ * @since 11/10/12
+ *        Time: 10:59 PM
+ */
+public class StubHibernateSessionFactory implements HibernateSessionFactory
+{
+    private final SessionFactory sessionFactory;
+
+    public StubHibernateSessionFactory(SessionFactory sessionFactory)
+    {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public SessionFactory getSessionFactory()
+    {
+        return sessionFactory;
+    }
+
+    @Override
+    public Session getCurrentSession()
+    {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public void start()
+    {
+    }
+
+    @Override
+    public void stop()
+    {
+    }
+}
